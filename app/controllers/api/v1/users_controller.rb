b6ecc7f0f3277c058cb2336a86572cc74b81d59c
goce-arsoski <goce_ars@yahoo.com>
2020-12-06 04:09:37 +0100

    First rails api

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::UsersController < ApplicationController
+  respond_to :json
+  skip_before_action :verify_authenticity_token
+
+  def show
+    respond_with User.find(params[:id])
+  end
+
+  # Creating users
+  def create
+    user = User.new(user_params)
+
+    if user.save
+      render json: user, status: 201
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  # Updating users
+  def update
+    user = User.find(params[:id])
+
+    if user.update(user_params)
+      render json: user, status: 200
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
+  # Deleting users
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    head 204
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+end
